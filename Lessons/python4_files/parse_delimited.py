6f3237df3e19d4dd9559b131125773686c4db4fd
Stephanie J Spielman <stephanie.spielman@gmail.com>
2015-02-23 18:57:06 -0600

    comment updates

--- Lessons/python4_files/parse_delimited.py
+++ Lessons/python4_files/parse_delimited.py
@@ -8,17 +8,19 @@ with open(infile_csv, 'r') as inf:
     reader = csv.reader(inf)
     for row in reader:
         print row
-    #inf.seek(0)
+    inf.seek(0) # required to loop another time. 
     for row in reader:
         print "NEW", row
 
-infile2 = "crabs_tdf.txt"
+# tab-delimited
 inf2 = open(infile_tdf, 'r')
 reader2 = csv.reader(inf2, delimiter = '\t')
 for row in reader2:
     print row
+
+# dictionaries instead of lists
 inf2.seek(0)
 reader_dict = csv.DictReader(inf2, delimiter = '\t')
 for row in reader_dict:
-    print row
+    print row # each row is a dictionary!
 inf2.close()
